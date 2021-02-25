27495557264a1ae9e9f326c12c35f14e13585c68
C-Monster-Boy <aranuj98@gmail.com>
2021-02-26 01:41:26 +0530

    Changes: - Set up first level - Create new materials - WorldStateManager (partial) - PlayerMovement

---
+++ World State Management/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Author: Anuj
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+
+    public void MovePlayer()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+
+        float xDelta = xInput * moveSpeed * Time.deltaTime;
+        float zDelta = yInput * moveSpeed * Time.deltaTime;
+
+        Vector3 moveDelta = new Vector3(xDelta, 0, zDelta);
+
+        transform.position += moveDelta;
+
+    }
+}
---
+++ World State Management/Assets/Scripts/WorldStateManager.cs
@@ -0,0 +1,73 @@
+﻿/*
+ * Author: Anuj
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldStateManager : MonoBehaviour
+{
+    public static WorldStateManager worldStateManagerInstance;
+    public float TimeOfCreation { get; private set; }
+
+    public WorldStates currentWorldState;
+    public AreaStateMapper[] areaStateList;
+
+    private void Awake()
+    {
+        if(worldStateManagerInstance)
+        {
+            DestroyNewerInstances();
+        }
+        else
+        {
+            TimeOfCreation = Time.time;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+   
+    private void DestroyNewerInstances()
+    {
+        WorldStateManager[] worldStateManagerList = GameObject.FindObjectsOfType<WorldStateManager>();
+
+        WorldStateManager oldest = worldStateManagerList[0];
+        for (int i = 1; i < worldStateManagerList.Length; i++)
+        {
+            if (worldStateManagerList[i].TimeOfCreation < oldest.TimeOfCreation)
+            {
+                oldest = worldStateManagerList[i];
+            }
+        }
+
+        for (int i = 0; i < worldStateManagerList.Length; i++)
+        {
+            if (worldStateManagerList[i] != oldest)
+            {
+                Destroy(worldStateManagerList[i].gameObject);
+            }
+        }
+    }
+}
+
+public enum WorldStates
+{
+    Start,
+    Earthquake
+}
+
+public enum AreaStates
+{
+    Pocco_Start,
+    Pocco_Earthquake,
+
+    PoccoHill_Start,
+    PoccoHill_Earthquake
+}
+
+[System.Serializable]
+public struct AreaStateMapper
+{
+    public string areaName;
+    public AreaStates currAreaState;
+}
