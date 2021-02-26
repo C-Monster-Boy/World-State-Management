6ebd8904a9b048d7ce81a33591de66e6140282ab
C-Monster-Boy <aranuj98@gmail.com>
2021-02-27 02:24:29 +0530

    Added Interact Prompt

--- World State Management/Assets/Scripts/Helpers/PlayerInteract.cs
+++ World State Management/Assets/Scripts/Helpers/PlayerInteract.cs
@@ -11,18 +11,23 @@ public class PlayerInteract : MonoBehaviour
     public Transform raycastOrigin;
     public float raycastLength;
 
+    [Header("Interact prompt object")]
+    public GameObject interactPrompt;
+
     private void Update()
     {
         RaycastHit ray;
 
         Vector3 direction = Vector3.Normalize(raycastOrigin.position - transform.position);
+
         if (Physics.Raycast(raycastOrigin.position, direction, out ray, raycastLength))
         {
-
             if(ray.collider.tag == "Interactable")
             {
-                //Press E to interact
+                interactPrompt.SetActive(true);
             }
+            
+
             if (Input.GetKeyDown(KeyCode.E))
             {
                 print(ray.collider.name);
@@ -37,11 +42,15 @@ public class PlayerInteract : MonoBehaviour
             }      
            
         }
+        else
+        {
+            interactPrompt.SetActive(false);
+        }
     }
 
     private void OnDrawGizmos()
     {
-        Vector3 direction = Vector3.Normalize(raycastOrigin.position - transform.position);
-        Gizmos.DrawWireSphere(raycastOrigin.position, raycastLength);
+        Vector3 direction = raycastOrigin.position - transform.position;
+        Gizmos.DrawRay(raycastOrigin.position, direction);
     }
 }
